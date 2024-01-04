76e6123fc1542096e5acd28c0e2725bc002959aa
Konstantin Klenitsky <kklenitsky@gmail.com>
2024-01-04 12:38:01 +0300

    Updated Contexts and created a migration
    Updated App Context to be synchronized with the app logic  and created a migration to synchronize database.

--- EmployeeService.DAL/Contexts/ApplicationDbContext.cs
+++ EmployeeService.DAL/Contexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeService.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,10 @@ namespace EmployeeService.DAL.Contexts
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<Country>?  Countries { get; set; }
+        public DbSet<Currency>? Currencies{ get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+        public DbSet<Office>? Offices { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options) { }
