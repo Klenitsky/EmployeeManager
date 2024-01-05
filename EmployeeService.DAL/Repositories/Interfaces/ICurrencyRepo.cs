ada62c3aaebf5d05f58d1e6ffe706d1fa75045c1
Konstantin Klenitsky <kklenitsky@gmail.com>
2024-01-05 12:21:44 +0300

    Added  repositories interfaces for concrete types

---
+++ EmployeeService.DAL/Repositories/Interfaces/ICurrencyRepo.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.DAL.Repositories.Interfaces
+{
+    public interface ICurrencyRepo: IRepo<Currency>
+    {
+    }
+}
