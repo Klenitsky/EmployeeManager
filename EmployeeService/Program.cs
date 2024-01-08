10fb547de96e4d9ea08bc0724a66bebc3e1efcc5
KKlenitsky <kklenitsky@gmail.com>
2024-01-08 11:16:21 +0300

    Used DI to add DbContext as Service

--- EmployeeService/Program.cs
+++ EmployeeService/Program.cs
@@ -1,3 +1,8 @@
+using EmployeeService.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("EmployeeDb");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
