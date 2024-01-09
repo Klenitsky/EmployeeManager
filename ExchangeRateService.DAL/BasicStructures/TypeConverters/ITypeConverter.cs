6f3b802ce39847246bb7f226a200a2f33e4ae348
KKlenitsky <kklenitsky@gmail.com>
2024-01-09 10:02:25 +0300

    Added Basic Entities
    Added model of exchange rate to store in database, added interface to convert rates from different API's to basic format of the system. Added folder structure

---
+++ ExchangeRateService.DAL/BasicStructures/TypeConverters/ITypeConverter.cs
@@ -0,0 +1,14 @@
+﻿using ExchangeRateService.DAL.BasicStructures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateService.DAL.BasicStructures.TypeFormatters
+{
+    public interface ITypeConverter<T>
+    {
+        public ExchangeRate ConvertToExchangeRate(T value);
+    }
+}
