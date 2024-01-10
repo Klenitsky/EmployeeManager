ef9191874d1137caddc24187d8475bb4c802bfd0
KKlenitsky <kklenitsky@gmail.com>
2024-01-10 10:25:08 +0300

    Created an interface of interaction with exchange Rates

---
+++ ExchangeRateService.DAL/DatabaseStructures/Repositories/IExchangeRateRepo.cs
@@ -0,0 +1,35 @@
+﻿using ExchangeRateService.DAL.BasicStructures.CurrencyLoaders.Interfaces;
+using ExchangeRateService.DAL.BasicStructures.Models;
+using ExchangeRateService.DAL.BasicStructures.RateReaders;
+using ExchangeRateService.DAL.BasicStructures.TypeFormatters;
+using ExchangeRateService.DAL.DatabaseStructures.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateService.DAL.DatabaseStructures.Repositories
+{
+    public interface IExchangeRateRepo<T>
+    {
+        protected ExchangeRateDbContext DbContext { get; set; }
+        protected ICurrencyLoader CurrencyLoader { get; set; }
+        protected IRateReader<T> RateReader { get; set; }
+        protected ITypeConverter<T> TypeConverter { get; set; }
+
+        public IEnumerable<ExchangeRate> GetExchangeRates();
+        public IEnumerable<ActiveCurrency> GetActiveCurrencies();
+
+        public IEnumerable<ExchangeRate> GetExchangeRatesByDate(DateTime date);
+        public IEnumerable<ExchangeRate> GetExchangeRatesByDateRange(DateTime startDate, DateTime endDate);
+
+        public IEnumerable<ExchangeRate> GetExchangeRatesByCurrency(string currencyAbbreviation);
+        public ExchangeRate GetExchangeRateByDateAndCurrency(string currencyAbbreviation, DateTime date);
+        public IEnumerable<ExchangeRate> GetExchangeRateByDateRangeAndCurrency(string currencyAbbreviation, DateTime startDate,DateTime endDate);
+
+        public void LoadActiveCurrencies() => CurrencyLoader.LoadCurrency(DbContext);
+
+
+    }
+}
