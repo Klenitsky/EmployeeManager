d8bdb27528008e6b4d1e82bb587b2b9c9587d56b
KKlenitsky <kklenitsky@gmail.com>
2024-01-12 12:09:49 +0300

    Implemented On request controller
    Implemented controller that gives exchange rates and if they are not in system, downloads them

--- ExchangeRateService/Controllers/ExchangeRateOnRequestController.cs
+++ ExchangeRateService/Controllers/ExchangeRateOnRequestController.cs
@@ -1,4 +1,5 @@
-﻿using ExchangeRateService.DAL.DatabaseStructures.Repositories;
+﻿using ExchangeRateService.DAL.BasicStructures.Models;
+using ExchangeRateService.DAL.DatabaseStructures.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,6 +16,62 @@ namespace ExchangeRateService.Controllers
         public ExchangeRateOnRequestController(IExchangeRateRepo repo)
         {
             _repo = repo;
+        }
+
+        [HttpGet("OnDate")]
+        public ActionResult<IEnumerable<ExchangeRate>> GetByDate(DateTime Date)
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRatesByDate(Date));
+            }
+            catch(Exception ex) 
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet("OnDateRange")]
+        public ActionResult<IEnumerable<ExchangeRate>> GetByDateRange(DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRatesByDateRange(startDate,endDate));
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet("OnDateAndCurrency")]
+        public ActionResult<ExchangeRate> GetByDateAndCurrency(DateTime date, string currencyAbbreviation)
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRateByDateAndCurrency(currencyAbbreviation,date));
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet("OnDateRangeAndCurrency")]
+        public ActionResult<IEnumerable<ExchangeRate>> GetByDateRangeAndCurrency(DateTime startDate, DateTime endDate, string currencyAbbreviation)
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRateByDateRangeAndCurrency(currencyAbbreviation,startDate, endDate));
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+
         }
     }
 }
