bbb8131838e4ca01f63931dccd9b746e559efb9b
KKlenitsky <kklenitsky@gmail.com>
2024-01-12 10:06:01 +0300

    Implemented InSystem rates controller
    Implemented controller that allows to work with exchange rates, which are already in service database

--- ExchangeRateService/Controllers/ExchangeRateInSystemController.cs
+++ ExchangeRateService/Controllers/ExchangeRateInSystemController.cs
@@ -1,4 +1,6 @@
-﻿using ExchangeRateService.DAL.DatabaseStructures.Repositories;
+﻿using EmployeeService.DAL.Models;
+using ExchangeRateService.DAL.BasicStructures.Models;
+using ExchangeRateService.DAL.DatabaseStructures.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -14,6 +16,60 @@ namespace ExchangeRateService.Controllers
         public ExchangeRateInSystemController(IExchangeRateRepo repo)
         {
             _repo = repo;
+        }
+
+        [HttpGet("rates")]
+        public ActionResult<IEnumerable<ExchangeRate>> GetExchangeRates()
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRates());
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet("currencies")]
+        public ActionResult<IEnumerable<ActiveCurrency>> GetActiveCurrencies()
+        {
+            try
+            {
+                return Ok(_repo.GetActiveCurrencies());
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("OnCurrency")]
+        ActionResult<IEnumerable<ExchangeRate>> GetRatesByCurrency(string currencyAbbreviation)
+        {
+            try
+            {
+                return Ok(_repo.GetExchangeRatesByCurrency(currencyAbbreviation));
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("LoadCurrencies")]
+        public IActionResult Add()
+        {
+            try
+            {
+                _repo.LoadActiveCurrencies();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
