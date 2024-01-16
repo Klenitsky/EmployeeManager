b7f4b6b20b0bcc17e80d31a44d389ef733b1b89d
KKlenitsky <kklenitsky@gmail.com>
2024-01-16 10:06:21 +0300

    Created Report Interface

---
+++ ReportService.Structures/Reports/Interfaces/IReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Structures.Reports.Interfaces
+{
+    public interface IReport
+    {
+        public void PrintToXML( string filename);
+        public void PrintToXLSX(string filename);
+        public void PrintToWord(string filename);
+        public void PrintToCSV(string filename);
+        public void PrintToPDF(string filename);
+        public void PrintToTXT(string filename);
+
+        public void LoadFromTXT(string filename);
+        public void LoadFromXLSX(string filename);
+    }
+}
