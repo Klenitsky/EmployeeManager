2a540b45e3c786f50457b7dbb531c35b6d13e857
KKlenitsky <kklenitsky@gmail.com>
2024-01-17 15:36:06 +0300

    Implemented Exchange Rate statistics Report

---
+++ ReportService.Structures/Reports/ExchangeRateOnDateRange/ExchangeRateOnDateRangeReport.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeService.DAL.Models;
+using ExchangeRateService.DAL.BasicStructures.Models;
+using ReportService.Structures.Reports.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Structures.Reports.ExchangeRateOnDateRange
+{
+    public class ExchangeRateOnDateRangeReport: IReport
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Dictionary<Currency,float> IncludedCurrencies { get; set; }
+        public List<ExchangeRate> ExchangeRates { get; set; }
+
+        public void PrintToCSV(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToPDF(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToTXT(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToWord(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToXLSX(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintToXML(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReportService.Structures/Reports/ExchangeRateOnDateRange/ExchangeRateOnDateRangeReportBuilder.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeService.DAL.Models;
+using ExchangeRateService.DAL.BasicStructures.Models;
+using ReportService.Structures.DataReaders;
+using ReportService.Structures.Reports.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Structures.Reports.ExchangeRateOnDateRange
+{
+    public class ExchangeRateOnDateRangeReportBuilder : IReportBuilder<ExchangeRateOnDateRangeReport>
+    {
+        private DateTime _startDate;
+        private DateTime _endDate { get; set; }
+        private Dictionary<Currency, float> _includedCurrencies { get; set; }
+        private List<ExchangeRate> _exchangeRates { get; set; }
+
+        private string _connectionStringEmployeeService;
+        private string _connectionStringExchangeRateService;
+
+        public ExchangeRateOnDateRangeReportBuilder(string EmployeeConnectionString, string ExchangeRateConnectionString)
+        {
+            _connectionStringExchangeRateService = ExchangeRateConnectionString;
+            _connectionStringEmployeeService = EmployeeConnectionString;
+        }
+
+        public void SetProperties(DateTime startDate, DateTime endDate, IEnumerable<Currency> includedCurrencies)
+        {
+            _startDate = startDate;
+            _endDate = endDate;
+            foreach(var currency in includedCurrencies)
+            {
+                _includedCurrencies.Add(currency, 0);
+            }
+        }
+        public void CountMetrics()
+        {
+            foreach(var currencyKey in _includedCurrencies.Keys)
+            {
+                _includedCurrencies[currencyKey] = _exchangeRates.Where(r=>r.Abbreviation == currencyKey.Abbreviation).Select(r=>r.Rate).Average();
+            }
+        }
+
+        public ExchangeRateOnDateRangeReport GetResult()
+        {
+            return new ExchangeRateOnDateRangeReport()
+            {
+                StartDate = _startDate,
+                EndDate = _endDate,
+                IncludedCurrencies = _includedCurrencies,
+                ExchangeRates = _exchangeRates,
+            };
+        }
+
+        public void LoadData()
+        {
+           ExchangeRateReader reader = new ExchangeRateReader(_connectionStringExchangeRateService);
+           foreach(var currency in _includedCurrencies)
+            {
+                IEnumerable<ExchangeRate> rates = reader.GetRatesOnDateRangeAndCurrency(_startDate, _endDate, currency.Key.Abbreviation);
+                _exchangeRates.AddRange(rates);
+            }
+        }
+    }
+}
