942524d3f2023c43e0e7b5f6a8c9a9c1a8e2659b
KKlenitsky <kklenitsky@gmail.com>
2024-02-06 11:37:08 +0300

    Created ExchangeRate Controller and Implemented Post Method
    Created Controller that will show exchange rate reports and implemented POST method to generate a report

---
+++ ReportService.Web/Controllers/ExchangeRateReportController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReportService.Structures.ParameterModels;
+using ReportService.Structures.Reports.ExchangeRateOnDateRange;
+using ReportService.Web.DataReaders.EmployeeServiceReaders;
+using ReportService.Web.DataReaders.ReportReaders;
+
+namespace ReportService.Web.Controllers
+{
+    public class ExchangeRateReportController : Controller
+    {
+
+
+        private ExchangeRateOnDateRangeReportReader _reportReader;
+        private CurrencyReader _currencyReader;
+
+        public ExchangeRateReportController(ExchangeRateOnDateRangeReportReader reportReader, CurrencyReader currencyReader)
+        {
+            _reportReader = reportReader;
+            _currencyReader = currencyReader;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var currencies = _currencyReader.Read();
+            ViewBag.Currencies = currencies.Select(c => c.Abbreviation).ToArray();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(DateTime startDate, DateTime endDate, string[] currencies)
+        {
+            var args = new ExchangeRateParametersModel()
+            {
+                StartDate = startDate,
+                EndDate = endDate,
+                IncludedCurrencies = _currencyReader.Read().Where(c => currencies.Contains(c.Abbreviation)).ToList(),
+            };
+            var report = _reportReader.GetReport(args);
+            return RedirectToAction("Show",report);
+        }
+
+
+        [HttpGet]
+        public IActionResult Show(ExchangeRateOnDateRangeReport report)
+        {
+            return View(report);
+        }
+
+
+
+    }
+}
