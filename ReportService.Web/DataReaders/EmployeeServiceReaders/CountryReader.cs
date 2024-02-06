2d452a5f7dc2a6d86de313bec35bc2b4c537244f
KKlenitsky <kklenitsky@gmail.com>
2024-02-06 14:23:46 +0300

    Implemented Data readers
    Implemented readers to read values that will be represented in forms to set the params of the report

---
+++ ReportService.Web/DataReaders/EmployeeServiceReaders/CountryReader.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeService.DAL.Models;
+
+namespace ReportService.Web.DataReaders.EmployeeServiceReaders
+{
+    public class CountryReader
+    {
+        private HttpClient _httpClient;
+        private string _connectionString;
+
+        public CountryReader(string connectionString)
+        {
+            _httpClient = new HttpClient();
+            _connectionString = connectionString + "EmployeeService/Country";
+        }
+
+        public IEnumerable<Country> Read()
+        {
+            List<Country> result = _httpClient.GetFromJsonAsync<IEnumerable<Country>>(_connectionString).Result.ToList();
+            return result;
+        }
+    }
+}
---
+++ ReportService.Web/DataReaders/EmployeeServiceReaders/OfficeReader.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeService.DAL.Models;
+
+namespace ReportService.Web.DataReaders.EmployeeServiceReaders
+{
+    public class OfficeReader
+    {
+        private HttpClient _httpClient;
+        private string _connectionString;
+
+        public OfficeReader(string connectionString)
+        {
+            _httpClient = new HttpClient();
+            _connectionString = connectionString + "EmployeeService/Office";
+        }
+
+        public IEnumerable<Office> Read()
+        {
+            List<Office> result = _httpClient.GetFromJsonAsync<IEnumerable<Office>>(_connectionString).Result.ToList();
+            return result;
+        }
+    }
+}
