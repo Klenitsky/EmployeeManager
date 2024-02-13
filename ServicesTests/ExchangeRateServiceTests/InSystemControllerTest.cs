214cca9f79afb28cb44a159fd607d5168cc7011d
KKlenitsky <kklenitsky@gmail.com>
2024-02-13 14:31:23 +0300

    Changed exchange rates service tests naming

--- ServicesTests/ExchangeRateServiceTests/InSystemControllerTest.cs
+++ ServicesTests/ExchangeRateServiceTests/InSystemControllerTest.cs
@@ -1,26 +1,70 @@
-﻿using ExchangeRateService.Controllers;
+﻿using EmployeeService.DAL.Contexts;
+using ExchangeRateService.Controllers;
+using ExchangeRateService.DAL.BasicStructures.Models;
+using ExchangeRateService.DAL.DatabaseStructures.Contexts;
 using ExchangeRateService.DAL.NbrbAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServicesTests.ExchangeRateServiceTests
 {
-    public class ExchangeRateServiceOnRequestTests
+    public class InSystemControllerTest
     {
-        private ExchangeRateOnRequestController _controller;
-
-        public ExchangeRateServiceOnRequestTests()
+        private ExchangeRateInSystemController _controller;
+   
+        public InSystemControllerTest()
         {
-            _controller = new ExchangeRateOnRequestController(new NbrbExchangeRateRepo());
+            _controller = new ExchangeRateInSystemController(new NbrbExchangeRateRepo());
         }
 
-        
+        [Fact]
+        public void GetAllRatesInSystemTest()
+        {
+           var result = _controller.GetExchangeRates().Value;
+           Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void GetActiveCurrenciesTest()
+        {
+            var result = _controller.GetActiveCurrencies().Value;
+            Assert.NotNull(result);
+        }
+
         [Theory]
-        [InlineData("01.05.2023")]
-        [InlineData("03.01.2024")]
+        [InlineData("USD")]
+        [InlineData("BYN")]
+        [InlineData("GBP")]
+        public void GetAllRatesByCurrencyTestSuccess(string abbreviation)
+        {
+            var result = _controller.GetRatesByCurrency(abbreviation).Value;
+            Assert.NotNull(result);
+            foreach(var rate in result)
+            {
+                Assert.NotNull(rate);
+                Assert.Equal(abbreviation, rate.Abbreviation);
+            }
+        }
+
+        [Theory]
+        [InlineData("sss")]
+        [InlineData("qqq")]
+        [InlineData("www")]
+        public void GetAllRatesByCurrencyTestBadCurrency(string abbreviation)
+        {
+            var result = _controller.GetRatesByCurrency(abbreviation).Value;
+            Assert.Null(result);
+        }
+
+
+        [Theory]
+        [InlineData("01.11.2023")]
+        [InlineData("03.02.2024")]
         public void OnDateTestSuccess(string dateString)
         {
             DateTime date = DateTime.Parse(dateString);
@@ -44,14 +88,15 @@ namespace ServicesTests.ExchangeRateServiceTests
             Assert.Null(result);
         }
 
+
         [Theory]
-        [InlineData("01.05.2023", "05.05.2023")]
-        [InlineData("03.01.2024", "05.01.2024")]
+        [InlineData("01.11.2023", "05.11.2023")]
+        [InlineData("03.02.2024","05.02.2024")]
         public void OnDateRangeTestSuccess(string startDateString, string endDateString)
         {
             DateTime startDate = DateTime.Parse(startDateString);
             DateTime endDate = DateTime.Parse(endDateString);
-            var result = _controller.GetByDateRange(startDate, endDate).Value;
+            var result = _controller.GetByDateRange(startDate,endDate).Value;
             Assert.NotNull(result);
             foreach (var rate in result)
             {
@@ -75,12 +120,12 @@ namespace ServicesTests.ExchangeRateServiceTests
 
 
         [Theory]
-        [InlineData("01.04.2023", "USD")]
-        [InlineData("03.01.2024", "GBP")]
+        [InlineData("01.11.2023", "USD")]
+        [InlineData("03.02.2024", "GBP")]
         public void OnDateAndCurrencyTestSuccess(string dateString, string abbreviation)
         {
             var date = DateTime.Parse(dateString);
-            var result = _controller.GetByDateAndCurrency(date, abbreviation).Value;
+            var result = _controller.GetByDateAndCurrency(date,abbreviation).Value;
             Assert.NotNull(result);
             Assert.Equal(date, result.Date);
             Assert.Equal(abbreviation, result.Abbreviation);
@@ -111,13 +156,13 @@ namespace ServicesTests.ExchangeRateServiceTests
 
 
         [Theory]
-        [InlineData("01.04.2023", "05.04.2023", "USD")]
-        [InlineData("03.01.2024", "05.01.2024", "GBP")]
+        [InlineData("01.11.2023", "05.11.2023","USD")]
+        [InlineData("03.02.2024", "05.02.2024","GBP")]
         public void OnDateRangeAndCurrencyTestSuccess(string startDateString, string endDateString, string abbreviation)
         {
             DateTime startDate = DateTime.Parse(startDateString);
             DateTime endDate = DateTime.Parse(endDateString);
-            var result = _controller.GetByDateRangeAndCurrency(startDate, endDate, abbreviation).Value;
+            var result = _controller.GetByDateRangeAndCurrency(startDate, endDate,abbreviation).Value;
             Assert.NotNull(result);
             foreach (var rate in result)
             {
@@ -129,9 +174,9 @@ namespace ServicesTests.ExchangeRateServiceTests
         }
 
         [Theory]
-        [InlineData("01.11.2015", "05.11.2015", "USD")]
-        [InlineData("03.02.2025", "05.02.2025", "USD")]
-        [InlineData("03.02.2024", "01.02.2024", "USD")]
+        [InlineData("01.11.2015", "05.11.2015","USD")]
+        [InlineData("03.02.2025", "05.02.2025","USD")]
+        [InlineData("03.02.2024", "01.02.2024","USD")]
         public void OnDateRangeAndCurrencyTestBadRange(string startDateString, string endDateString, string abbreviation)
         {
             DateTime startDate = DateTime.Parse(startDateString);
@@ -150,7 +195,5 @@ namespace ServicesTests.ExchangeRateServiceTests
             var result = _controller.GetByDateRangeAndCurrency(startDate, endDate, abbreviation).Value;
             Assert.Null(result);
         }
-
-
     }
 }
