c735fa3e51d6b46f7f08d766f7f96e19c152d366
KKlenitsky <kklenitsky@gmail.com>
2024-02-14 15:46:47 +0300

    Implemented Salary Summary Report Controller tests

--- ServicesTests/ReportServiceTests/SalarySummaryReportControllerTest.cs
+++ ServicesTests/ReportServiceTests/SalarySummaryReportControllerTest.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using EmployeeService.Controllers;
+using EmployeeService.DAL.Contexts;
+using EmployeeService.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using ReportService.Controllers;
+using ReportService.Structures.ParameterModels;
+using ReportService.Structures.Reports.PaymentOnDateRange;
+using ReportService.Structures.Reports.SalarySummary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +16,94 @@ namespace ServicesTests.ReportServiceTests
 {
     public class SalarySummaryReportControllerTest
     {
+        private SalarySummaryReportController _SalarySummaryReportController;
+        private List<Office> _includedOffices;
+        private List<Country> _includedCountries;
+
+
+        public SalarySummaryReportControllerTest()
+        {
+            _SalarySummaryReportController = new SalarySummaryReportController(new SalarySummaryReportBuilder("https://localhost:44316/api/EmployeeService", "https://localhost:44341/api/ExchangeRate"));
+            string[] args = { };
+            _includedOffices = new List<Office>
+            {
+                    new OfficeController(new ApplicationDbContextFactory().CreateDbContext(args)).Find(1).Value
+            };
+
+            _includedCountries = new List<Country>
+            {
+                    new CountryController(new ApplicationDbContextFactory().CreateDbContext(args)).Find(2).Value
+            };
+
+        }
+
+
+        [Fact]
+        public void GenerateReportSuccessTest()
+        {
+            SalarySummaryParametersModel parametersModel = new SalarySummaryParametersModel()
+            {
+                Date = new DateTime(2023, 11, 1),
+                IncludedCountries = _includedCountries,
+                IncludedOffices = _includedOffices
+            };
+            var result = _SalarySummaryReportController.SetParameters(parametersModel);
+            Assert.True(result is OkResult);
+
+            var report = _SalarySummaryReportController.GetOnDateRange().Value;
+            Assert.NotNull(report);
+            Assert.Equal(new DateTime(2023, 11, 1), report.Date);
+            foreach (var includedCountry in _includedCountries)
+            {
+                Assert.Contains(includedCountry, report.IncludedCountriesMetrics.Keys.ToList());
+            }
+            foreach (var includedOffice in _includedOffices)
+            {
+                Assert.Contains(includedOffice, report.IncludedOfficesMetrics.Keys.ToList());
+            }
+        }
+
+        [Fact]
+        public void GenerateReportBadRangeDate()
+        {
+            SalarySummaryParametersModel parametersModel = new SalarySummaryParametersModel()
+            {
+                Date = new DateTime(2025, 11, 1),
+                IncludedCountries = _includedCountries,
+                IncludedOffices = _includedOffices
+            };
+            var result = _SalarySummaryReportController.SetParameters(parametersModel);
+            Assert.False(result is OkResult);
+        }
+
+        [Fact]
+        public void GenerateReportBadOfficeTest()
+        {
+            var offices = _includedOffices.ToList();
+            offices.Add(new Office { Name = "qwe", CountryId = 1000 });
+            SalarySummaryParametersModel parametersModel = new SalarySummaryParametersModel()
+            {
+                Date = new DateTime(2023, 11, 1),
+                IncludedCountries = _includedCountries,
+                IncludedOffices = offices
+            };
+            var result = _SalarySummaryReportController.SetParameters(parametersModel);
+            Assert.False(result is OkResult);
+        }
+
+        [Fact]
+        public void GenerateReportBadCountryTest()
+        {
+            var countries = _includedCountries.ToList();
+            countries.Add(new Country { Name = "avs", Abbreviation = "aaa", CurrencyId = 1000 });
+            SalarySummaryParametersModel parametersModel = new SalarySummaryParametersModel()
+            {
+                Date = new DateTime(2023, 11, 1),
+                IncludedCountries = countries,
+                IncludedOffices = _includedOffices
+            };
+            var result = _SalarySummaryReportController.SetParameters(parametersModel);
+            Assert.False(result is OkResult);
+        }
     }
 }
